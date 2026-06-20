4b80875e9f310c2ebd9c18739d57fcb9df362f6f
lucaswnn <lucaswnn@hotmail.com>
2026-06-20 11:21:37 -0300

    feat: captura PRs de um repositório em get_prs em github_client.py

--- pr_debt_scanner/github_client.py
+++ pr_debt_scanner/github_client.py
@@ -1,7 +1,9 @@
 # pr_debt_scanner/github_client.py
 import os
 from github import Github, GithubException, Auth
+from github.PullRequest import PullRequest
 from dotenv import load_dotenv
+from typing import Iterable
 
 load_dotenv()
 
@@ -37,6 +39,32 @@ def get_pr_files(repo_name: str, pr_number: int) -> list:
         repo = client.get_repo(repo_name)
         pr = repo.get_pull(pr_number)
         return list(pr.get_files())
+    except GithubException as e:
+        raise SystemExit(
+            f"Erro ao acessar o GitHub: {e.data.get('message', str(e))}")
+
+def get_pr_files_from_pr(pr: PullRequest) -> list:
+    return list(pr.get_files())
+
+def get_prs(repo_name: str, include_closeds: bool = False) -> Iterable[PullRequest]:
+    """
+    Busca a lista de Pull Requests de um repositório.
+
+    Args:
+        repo_name: No formato "owner/repo" (ex: "torvalds/linux")
+        include_closeds: True para incluir Pull Requests fechadas
+
+    Returns:
+        Lista de objetos PullRequestFile do PyGithub.
+
+    Raises:
+        SystemExit: Se o repo ou PR não existirem, ou o token for inválido.
+    """
+    try:
+        client = get_github_client()
+        repo = client.get_repo(repo_name)
+        state = 'all' if include_closeds else 'open'
+        return repo.get_pulls(state=state)
     except GithubException as e:
         raise SystemExit(
             f"Erro ao acessar o GitHub: {e.data.get('message', str(e))}")
